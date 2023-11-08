95b3abaa723e95f785ea5581627b81e52ed3c3f9
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-11-08 20:14:52 +0500

    Update the api link and make responsive

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,6 +1,6 @@
 import Score from './score.js';
 
-const api = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/u7kNvsXmXdu1EufOu2CH/scores/';
+const api = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/u7kNvsXmedu1EufOu2CH/scores/';
 
 class UI {
   static displayScores = async () => {
